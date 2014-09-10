cb6bc9a27a23f9fefcd73cb729a4c8fd68bd719d
ssrinidhi <snehasrinidhi@gmail.com>
2014-09-10 13:24:12 -0700

    first git scala program

---
+++ src/main/scala/hello/SampleController.scala
@@ -0,0 +1,23 @@
+package hello
+
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.web.bind.annotation._
+
+
+object SampleController {
+
+  def main(args: Array[String]) {
+    SpringApplication.run(classOf[SampleController])
+  }
+}
+
+@Controller
+@EnableAutoConfiguration
+class SampleController {
+
+  @RequestMapping(Array("/"))
+  @ResponseBody
+  def home(): String = "Hello World!"
+}
